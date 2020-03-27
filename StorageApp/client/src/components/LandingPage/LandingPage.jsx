0efc8321b36dbc24d891a6a54a96c20b997a0414
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-03-27 23:15:13 +0100

    commit

---
+++ StorageApp/client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom'
+import { GiAlarmClock } from 'react-icons/gi';
+import { FaClipboardList, FaShippingFast, FaDatabase } from 'react-icons/fa';
+
+const LandingPage = () => {
+
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if(hours > 5 && hours < 12){
+        timeOfDay = "Добро Утро";
+    }else if(hours > 12 && hours < 17){
+        timeOfDay = "Здравей";
+    }else{
+        timeOfDay = "Добър Вечер";
+    }
+
+
+    return(
+      <Fragment>
+        <main className='homeMain'>
+            <div className='leftSection'></div>
+            <aside className='rightSection'>
+              <div>
+                <h1>{timeOfDay}, заповядай<span>в нашата платформа</span></h1>
+                <p>За да използваш платформата за поръчки първо трябва да създадеш
+                    свои парола и потребителско име.
+                </p>
+                <div className='button'>
+                  <Link to='/register'><button>Регистрирай се</button></Link>
+                </div>               
+                <p>Ако вече имаш Регистрация <Link to='/login' style={{ color: 'green', textDecoration:'none' }}> Натиснете тук</Link> </p>
+              </div>
+            </aside>      
+        </main>
+        <footer className='homeFooter'>
+            <div className='infoCard'>
+                <i><GiAlarmClock /></i>
+                <p>Нашата платформа ще ви спести време</p>
+            </div> 
+            <div className='infoCard'>
+                <i><FaShippingFast /></i>
+                <p>Можете да коригирате заявките си докато ги изпълнявате в реално време</p>
+            </div> 
+            <div className='infoCard'>
+                <i><FaClipboardList /></i>
+                <p>Всичките Ви заявки са подредени на едно място</p>
+            </div> 
+            <div className='infoCard'>
+                <i><FaDatabase /></i>
+                    <p>Лесен и бърз достъп до вашите дани и обработката им</p>
+            </div> 
+        </footer>
+      </Fragment>
+    )
+}
+
+export default LandingPage;
