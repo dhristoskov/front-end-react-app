466fc97f8b032ddefcdf47d8177fb9838bfd8830
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-03-27 23:14:56 +0100

    commit
    info messages

---
+++ StorageApp/client/src/components/InfoBords/RequestInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RequestInfo = () => {
+
+    return(
+        <div className='infoDiv'>Тук можете да създадете своята заявка, като дадете име,
+             количество цена и т.н.Също така да изберете за кой 
+             обект се отнася тя.</div>
+    )
+}
+
+export default RequestInfo;
---
+++ StorageApp/client/src/components/InfoBords/ShopInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopInfo = () => {
+
+    return(
+        <div className='infoDiv'>Тук можете да зададете име на своя търговски обект,
+         както и кратко описание за него.</div>
+    )
+}
+
+export default ShopInfo;
